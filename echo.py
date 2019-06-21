7f123d4d56246e758146d07bba903ec43a15f18e
Survi <suravitaroy@gmail.com>
2019-06-21 10:58:09 -0400

    updated test_echo.py

--- echo.py
+++ echo.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 """An enhanced version of the 'echo' cmd line utility"""
-
-__author__ = "jupiter2018"
-
-
 import sys
 import argparse
+__author__ = "jupiter2018"
 
 
 def create_parser():
     """Creates and returns an argparse cmd line option parser"""
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description='Perform transformation on input text.')
     parser.add_argument(
-        '-u' , '--upper', help='convert text to uppercase', action='store_true')
+        '-u', '--upper', help='convert text to uppercase', action='store_true')
     parser.add_argument(
-        '-l' , '--lower', help='convert text to lowercase', action='store_true')
+        '-l', '--lower', help='convert text to lowercase', action='store_true')
     parser.add_argument(
-        '-t', '--title',  help='convert text to titlecase', action='store_true')
+        '-t', '--title', help='convert text to titlecase', action='store_true')
     parser.add_argument(
         'text', help='text to be manipulated')
-    
-    
+
     return parser
-    
 
 
 def main(args):
@@ -33,12 +30,12 @@ def main(args):
     print(namespace)
     curtext = namespace.text
     if namespace.upper:
-        return curtext.upper()
+        curtext = curtext.upper()
     if namespace.lower:
-        return curtext.lower()
+        curtext = curtext.lower()
     if namespace.title:
-        return curtext.title()
-
+        curtext = curtext.title()
+    return curtext
 
 
 if __name__ == '__main__':
--- tests/test_echo.py
+++ tests/test_echo.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import sys
-#sys.path.append('../')
+
 import unittest
 import echo
 import subprocess
 
 # Your test case class goes here
+
+
 class TestEcho(unittest.TestCase):
     def setUp(self):
         self.parser = echo.create_parser()
-
 
     def test_help(self):
         """ Running the program without arguments should show usage. """
@@ -27,46 +27,35 @@ class TestEcho(unittest.TestCase):
 
     def test_upper(self):
         """ Running the program with -u and changing text to uppercase """
-        arg_list = ['-u','hello']
+        arg_list = ['--upper', 'hello']
         parsed = self.parser.parse_args(arg_list)
         self.assertTrue(parsed.upper)
-        self.assertEqual(echo.main(arg_list),'HELLO')
-    
+        self.assertEqual(echo.main(arg_list), 'HELLO')
+
     def test_lower(self):
         """ Running the program with -l and changing text to lowercase """
-        arg_list = ['-l','Hello']
+        arg_list = ['--lower', 'Hello']
         parsed = self.parser.parse_args(arg_list)
         self.assertEqual(parsed.lower, True)
-        self.assertEqual(echo.main(arg_list),'Hello')
+        self.assertEqual(echo.main(arg_list), 'hello')
 
     def test_title(self):
         """ Running the program with -t and changing text to capitalize """
-        arg_list = ['-t', 'hELLo']
-        parsed = self.parser.parse_args()
+        arg_list = ['--title', 'hELLo']
+        parsed = self.parser.parse_args(arg_list)
+        print(parsed)
         self.assertTrue(parsed.title)
-        self.assertEqual(echo.main(arg_list),'Hello')
+        self.assertEqual(echo.main(arg_list), 'Hello')
 
     def test_alloptions(self):
         """ Check when all options are provided """
         arg_list = ['-tul', 'hELLo']
         parsed = self.parser.parse_args(arg_list)
-        self.assertEqual(parsed.title,True)
-        self.assertEqual(parsed.upper,True)
-        self.assertEqual(parsed.lower,True)
-        self.assertEqual(echo.main(arg_list),'hELLo')
+        self.assertEqual(parsed.title, True)
+        self.assertEqual(parsed.upper, True)
+        self.assertEqual(parsed.lower, True)
+        self.assertEqual(echo.main(arg_list), 'Hello')
 
-
-        
-    def test_nooptions(self):
-        """ Check when no options are provided """
-        parsed = self.parser.parse_args()
-        if(not parsed.upper and not parsed.lower and not parsed.title and parsed.text):
-            self.assertEqual(echo.main(),'hello')
-
-    
 
 if __name__ == '__main__':
     unittest.main()
-
-
-
